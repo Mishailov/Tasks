41308f70e455b57fe09a21b00c24382bd67062ce
Uladislau Mikhailau <17it3.mikhailau.u@pdu.by>
2021-04-08 15:13:11 +0300

    With TryParse

--- FirstTask/Coordinate.cs
+++ FirstTask/Coordinate.cs
@@ -6,28 +6,37 @@ namespace FirstTask
 {
     class Coordinate
     {
-        public List<string> ListCoordinate { get; set; }
+        private List<string> _listCoordinate;
 
         public Coordinate(List<string> listCoordinate)
         {
-            ListCoordinate = listCoordinate;
+            _listCoordinate = listCoordinate;
         }
 
-        public override string ToString()
+        private string GetParseToFloatOrEmptyString()
         {
             string coordinatesAfterValid = string.Empty;
-            foreach (var item in ListCoordinate)
+            foreach (var item in _listCoordinate)
             {
                 string[] coordinate = item.Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries);
-                if(coordinate.Length == 2)
+                if (coordinate.Length == 2)
                 {
-                    float x = float.Parse(coordinate[0].Replace('.', ','));
-                    float y = float.Parse(coordinate[1].Replace('.', ','));
-                    coordinatesAfterValid += $"X: {x} Y: {y}" + "\n";
+                    if(float.TryParse(coordinate[0].Replace('.', ','), out float x)
+                        && float.TryParse(coordinate[1].Replace('.', ','), out float y))
+                    {
+                        x = float.Parse(coordinate[0].Replace('.', ','));
+                        y = float.Parse(coordinate[1].Replace('.', ','));
+                        coordinatesAfterValid += $"X: {x} Y: {y}" + "\n";
+                    }
                 }
             }
 
             return coordinatesAfterValid;
+        }
+
+        public override string ToString()
+        {
+            return GetParseToFloatOrEmptyString();
         }
     }
 }
