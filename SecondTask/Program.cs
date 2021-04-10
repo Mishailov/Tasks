7325c123c14849a54834befa8d1ba6c45622a905
Uladislau Mikhailau <17it3.mikhailau.u@pdu.by>
2021-04-10 17:38:27 +0300

    Second Task

---
+++ SecondTask/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace SecondTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double input = 0.0;
+            int rootNumber = 0;
+            double epsil = 0.0;
+
+            Console.WriteLine("Write number for root");
+            string inputNum = Console.ReadLine();
+
+            Console.WriteLine("Write root");
+            string rootNum = Console.ReadLine();
+
+            Console.WriteLine("write eps");
+            string eps = Console.ReadLine();
+
+            if(TryParsing(inputNum) && TryParsing(eps))
+            {
+                input = double.Parse(inputNum);
+                epsil = double.Parse(eps);
+            }
+            else
+                Console.WriteLine("Uncorrect double value");
+
+            if (int.TryParse(rootNum, out int num))
+                rootNumber = int.Parse(rootNum);
+            else
+                Console.WriteLine("Uncorrect root");
+
+            Sqrt sqrt = new Sqrt(input, rootNumber, epsil);
+            Console.WriteLine(sqrt.GetSqrtByNewton().ToString());
+            Console.WriteLine(sqrt.GetCompareWithPowValue().ToString());
+        }
+
+        static bool TryParsing(string input)
+        {
+            return (double.TryParse(input, out double num));
+        }
+    }
+}
---
+++ SecondTask/Sqrt.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SecondTask
+{
+    class Sqrt
+    {
+        private double _inputNum;
+        private int _rootNum;
+        private double _eps;
+
+        public Sqrt(double inputNum, int rootNum, double eps)
+        {
+            _inputNum = inputNum;
+            _rootNum = rootNum;
+            _eps = eps;
+        }
+
+        public double GetSqrtByNewton()
+        {
+            if (_eps >= 1)
+                _eps = Double.Epsilon;
+
+            if (_rootNum < 2)
+                return _inputNum;
+
+            if (_inputNum < 0 && _rootNum % 2 == 0)
+                return Double.NaN;
+
+            double xk1 = _inputNum;
+            double xk = 0;
+            for (; ; )
+            {
+                xk1 = (1.0 / _rootNum) * ((_rootNum - 1) * xk1 + _inputNum / (Math.Pow(xk1, _rootNum - 1)));
+                if (Math.Abs(xk - xk1) <= _eps) break;
+                xk = xk1;
+            }
+
+            return (xk1 - xk1 % _eps);
+        }
+
+        public bool GetCompareWithPowValue()
+        {
+            if (_eps >= 1)
+                _eps = Double.Epsilon;
+            double result = Math.Pow(_inputNum, 1 / (double)_rootNum)
+                - Math.Pow(_inputNum, 1 / (double)_rootNum) % _eps;
+
+            return result == GetSqrtByNewton();
+        }
+    }
+}
