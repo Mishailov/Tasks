115e02a8fbfb6a3e20138d17dd48eb42bd8570a2
Uladislau Mikhailau <17it3.mikhailau.u@pdu.by>
2021-05-18 13:55:51 +0300

    linq

--- FifthTask/Vector3D.cs
+++ FifthTask/Vector3D.cs
@@ -1,62 +1,92 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
 using System.Text;
 
 namespace FifthTask
 {
     public class Vector3D
     {
-        private double _x;
-        private double _y;
-        private double _z;
+        private List<double> coordinates = new List<double>();
 
         public Vector3D(double x, double y, double z)
         {
-            _x = x;
-            _y = y;
-            _z = z;
+            coordinates.Add(x);
+            coordinates.Add(y);
+            coordinates.Add(z);
         }    
 
         public Vector3D Add(Vector3D vector)
         {
             return this + vector;
-            //return new Vector3D(_x + vector._x, _y + vector._y, _z + vector._z);
         }
 
         public static Vector3D operator +(Vector3D vector1, Vector3D vector2)
         {
-            return new Vector3D(vector1._x + vector2._x, vector1._y + vector2._y, vector1._z + vector2._z);
+            List<double> sum = new List<double>();
+            int index = 0;
+            foreach (var item in vector1.coordinates)
+            {
+                sum.Add(item + vector2.coordinates[index]);
+                index++;
+            }
+            return new Vector3D(sum[0], sum[1], sum[2]);
         }
 
         public Vector3D Subtract(Vector3D vector)
         {
             return this - vector;
-            //return new Vector3D(_x - vector._x, _y - vector._y, _z - vector._z);
         }
 
         public static Vector3D operator -(Vector3D vector1, Vector3D vector2)
         {
-            return new Vector3D(vector1._x - vector2._x, vector1._y - vector2._y, vector1._z - vector2._z);
+            List<double> subtract = new List<double>();
+            int index = 0;
+            foreach (var item in vector1.coordinates)
+            {
+                subtract.Add(item - vector2.coordinates[index]);
+                index++;
+            }
+            return new Vector3D(subtract[0], subtract[1], subtract[2]);
         }
 
         public Vector3D Multiply(Vector3D vector)
         {
             return this * vector;
-            //return new Vector3D((_y * vector._z) - (_z * vector._y)
-            //    , (_z * vector._x) - (_x * vector._z)
-            //    , (_x * vector._y) - (_y * vector._x));
         }
 
         public static Vector3D operator *(Vector3D vector1, Vector3D vector2)
         {
-            return new Vector3D((vector1._y * vector2._z) - (vector1._z * vector2._y)
-                , (vector1._z * vector2._x) - (vector1._x * vector2._z)
-                , (vector1._x * vector2._y) - (vector1._y * vector2._x));
+            List<double> mult = new List<double>();
+
+            //foreach in foreach is not a good idea, i know that)
+            //but this logic has the right to life? i think)
+            foreach (var item in vector1.coordinates)
+            {
+                var p = vector2.coordinates.Where(x => vector2.coordinates.IndexOf(x) 
+                    != vector1.coordinates.IndexOf(item)).ToList();
+                p = p.Select(x => x * item).ToList();
+                foreach(var it in p)
+                {
+                    mult.Add(it);
+                }
+            }
+
+            //this hard code only for check
+            mult.Add(mult[0] - mult[2]);
+            mult.Add(mult[4] - mult[1]);
+            mult.Add(mult[3] - mult[5]);
+            mult.RemoveRange(0, 6);
+            return new Vector3D(mult[0], mult[1], mult[2]);
+            //return new Vector3D((vector1._y * vector2._z) - (vector1._z * vector2._y)
+            //    , (vector1._z * vector2._x) - (vector1._x * vector2._z)
+            //    , (vector1._x * vector2._y) - (vector1._y * vector2._x));
         }
 
         public override string ToString()
         {
-            return string.Format("X:{0}, Y:{1}, Z:{2}", _x, _y, _z);
+            return string.Format("X:{0}, Y:{1}, Z:{2}", coordinates[0], coordinates[1], coordinates[2]);
         }
     }
 }
