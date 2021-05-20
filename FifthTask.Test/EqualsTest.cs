341f549894dfd645e2cc61f114e839ce0320f4e0
Uladislau Mikhailau <17it3.mikhailau.u@pdu.by>
2021-05-20 12:56:01 +0300

    add equalsTest

---
+++ FifthTask.Test/EqualsTest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+
+namespace FifthTask.Test
+{
+    class EqualsTest : IEqualityComparer<Vector3D>
+    {
+        public bool Equals(Vector3D x, Vector3D y)
+        {
+            if(x is null || y is null)
+                return false;
+
+            if (!x.coordinates.ToList().SequenceEqual(y.coordinates.ToList()))
+                return false;
+            
+            return true;
+        }
+
+        public int GetHashCode(Vector3D obj)
+        {
+            return 0;
+        }
+    }
+}
