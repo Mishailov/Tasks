de53575ef0344ddd3d6ef0403703fdf3972f6c6f
Uladislau Mikhailau <17it3.mikhailau.u@pdu.by>
2021-06-30 17:29:18 +0300

    tests

---
+++ EighthTask/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace EighthTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<uint> list = new List<uint>(4);
+            list.Add(0);
+            list.Add(0);
+            list.Add(0);
+            list.Add(4);
+            Timer timer = new Timer(list);
+            timer.StoppedTimer += c_StoppedTimer;
+            timer.TimerStarted();
+        }
+
+        static void c_StoppedTimer(object sender, EventArgs e)
+        {
+            Console.WriteLine("Time is over");
+            Environment.Exit(0);
+        }
+    }
+}
---
+++ EighthTask/Timer.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace EighthTask
+{
+    class Timer
+    {
+        private uint _day;
+        private uint _hour;
+        private uint _minuts;
+        private uint _seconds;
+
+        public event EventHandler StoppedTimer;
+
+        public Timer(List<uint> times)
+        {
+            if (times.Count != 4)
+                throw new ArgumentOutOfRangeException("more or less than four");
+
+            _day = times[0];
+            _hour = times[1];
+            _minuts = times[2];
+            _seconds = times[3];
+        }
+
+        public void TimerStarted()
+        {
+            for (uint i = 0; i < ConvertTimeToSeconds(); i++)
+            {
+                Thread.Sleep(1000);
+            }
+
+            StoppedTimer?.Invoke(this, EventArgs.Empty);
+        }
+
+        private uint ConvertTimeToSeconds()
+        {
+            return (((((_day * 24) + _hour) * 60) + _minuts) * 60) + _seconds;
+        }
+    }
+}
