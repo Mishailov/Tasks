98ee29427393ae0f3cbbedbdc2b0b0ab3a69c84c
Uladislau Mikhailau <17it3.mikhailau.u@pdu.by>
2021-07-14 12:08:10 +0300

    no message

---
+++ ninthTask/BinaryTree.cs
@@ -0,0 +1,182 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ninthTask
+{
+    class BinaryTree<T> where T : IComparable
+    {
+        class BinaryTreeNode
+        {
+            public BinaryTreeNode Left;
+            public BinaryTreeNode Right;
+            public BinaryTreeNode Parent;
+            public T Data;
+
+            public BinaryTreeNode()
+            {
+                Left = null;
+                Right = null;
+                Parent = null;
+            }
+        }
+
+        BinaryTreeNode Root;
+        Comparison<T> CompareFunction;
+
+        public BinaryTree(Comparison<T> theCompareFunction)
+        {
+            Root = null;
+            CompareFunction = theCompareFunction;
+        }
+
+        public static int CompareFunction_Int(int left, int right)
+        {
+            return left - right;
+        }
+
+        public static int CompareFunction_String(string left, string right)
+        {
+            return left.CompareTo(right);
+        }
+
+        public void Add(T Value)
+        {
+            BinaryTreeNode child = new BinaryTreeNode();
+            child.Data = Value;
+
+            if (Root == null)
+            {
+                Root = child;
+            }
+            else
+            {
+                BinaryTreeNode Iterator = Root;
+                while (true)
+                {
+                    int Compare = CompareFunction(Value, Iterator.Data);
+
+                    if (Compare <= 0)
+                        if (Iterator.Left != null)
+                        {
+                            Iterator = Iterator.Left;
+                            continue;
+                        }
+                        else
+                        {
+                            Iterator.Left = child;
+                            child.Parent = Iterator;
+                            break;
+                        }
+                    if (Compare > 0)
+                        if (Iterator.Right != null)
+                        {
+                            Iterator = Iterator.Right;
+                            continue;
+                        }
+                        else
+                        {
+                            Iterator.Right = child;
+                            child.Parent = Iterator;
+                            break;
+                        }
+                }
+
+            }
+
+        }
+
+        public bool Find(T Value)
+        {
+            BinaryTreeNode Iterator = Root;
+            while (Iterator != null)
+            {
+                int Compare = CompareFunction(Value, Iterator.Data);
+
+                if (Compare == 0) return true;
+                if (Compare < 0)
+                {
+                    Iterator = Iterator.Left;
+                    continue;
+                }
+                Iterator = Iterator.Right;
+            }
+            return false;
+        }
+
+        BinaryTreeNode FindMostLeft(BinaryTreeNode start)
+        {
+            BinaryTreeNode node = start;
+            while (true)
+            {
+                if (node.Left != null)
+                {
+                    node = node.Left;
+                    continue;
+                }
+                break;
+            }
+            return node;
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            return new BinaryTreeEnumerator(this);
+        }
+
+        class BinaryTreeEnumerator : IEnumerator<T>
+        {
+            BinaryTreeNode current;
+            BinaryTree<T> theTree;
+
+            public BinaryTreeEnumerator(BinaryTree<T> tree)
+            {
+                theTree = tree;
+                current = null;
+            }
+
+            public bool MoveNext()
+            {
+                if (current == null)
+                    current = theTree.FindMostLeft(theTree.Root);
+                else
+                {
+                    if (current.Right != null)
+                        current = theTree.FindMostLeft(current.Right);
+                    else
+                    {
+                        T CurrentValue = current.Data;
+
+                        while (current != null)
+                        {
+                            current = current.Parent;
+                            if (current != null)
+                            {
+                                int Compare = theTree.CompareFunction(current.Data, CurrentValue);
+                                if (Compare < 0) continue;
+                            }
+                            break;
+                        }
+
+                    }
+                }
+                return (current != null);
+            }
+
+            public T Current
+            {
+                get
+                {
+                    if (current == null)
+                        throw new InvalidOperationException();
+                    return current.Data;
+                }
+            }
+
+            object System.Collections.IEnumerator.Current => throw new NotImplementedException();
+
+            public void Dispose() { }
+            public void Reset() { current = null; }
+        }
+    }
+}
---
+++ ninthTask/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ninthTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BinaryTree<int> Test = new BinaryTree<int>(BinaryTree<int>.CompareFunction_Int);
+
+            // Build the tree
+            Test.Add(5);
+            Test.Add(2);
+            Test.Add(1);
+            Test.Add(3);
+            Test.Add(3); // Duplicates are OK
+            Test.Add(4);
+            Test.Add(6);
+            Test.Add(10);
+            Test.Add(7);
+            Test.Add(8);
+            Test.Add(9);
+
+            foreach (int value in Test)
+            {
+                Console.WriteLine("Value: {0}", value);
+            }
+        }
+    }
+}
